32d689d59caed6d720276578728cb3db42c83d22
ANDREW MAXWELL <amaxwell01@AndrewMaxwell2.local>
2012-12-05 22:20:30 -0800

    Created a new sidebar file for the blog and finalized the content for the footer and standard sidebar

---
+++ sidebar_blog.php
@@ -0,0 +1,32 @@
+<section>
+    <h1 class="section_header">Categories</h1>
+    <nav>
+        <ul>
+            <li><a href="x">Design</a></li>
+            <li><a href="x">Education</a></li>
+            <li><a href="x">General</a></li>
+            <li><a href="x">Ideas</a></li>
+            <li><a href="x">Organization</a></li>
+            <li><a href="x">Technology and Web</a></li>
+        </ul>
+    </nav>
+</section>
+<section>
+    <h1 class="section_header">Archives</h1>
+    <nav>
+        <ul>
+            <li><a href="x">2012</a>
+                <ul>
+                    <li><a href="x">January</a></li>
+                    <li><a href="x">February</a></li>
+                    <li><a href="x">March</a></li>
+                    <li><a href="x">April</a></li>
+                    <li><a href="x">May</a></li>
+                </ul>
+            </li>
+            <li><a href="x">2011</a></li>
+            <li><a href="x">2010</a></li>
+            <li><a href="x">2009</a></li>
+        </ul>
+    </nav>
+</section>
